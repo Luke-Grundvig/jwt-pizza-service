789a27698157ab0ab50a10f9548058b80f2f15ee
Luke Grundvig <lukevig@gmail.com>
2025-02-04 20:57:34 -0700

    Got working tests for authRouter, adding franchiseRouter tests next

--- src/routes/authRouter.test.js
+++ src/routes/authRouter.test.js
@@ -1,14 +1,20 @@
 const request = require('supertest');
 const app = require('../service');
+const { Role, DB } = require('../database/database.js');
 
 const testUser = { name: 'pizza diner', email: 'reg@test.com', password: 'a' };
 let testUserAuthToken;
+let registerRes;
+let adminLoginRes;
 
 beforeAll(async () => {
   testUser.email = Math.random().toString(36).substring(2, 12) + '@test.com';
-  const registerRes = await request(app).post('/api/auth').send(testUser);
+  registerRes = await request(app).post('/api/auth').send(testUser);
   testUserAuthToken = registerRes.body.token;
   expectValidJwt(testUserAuthToken);
+
+  const admin = await createAdminUser();
+  adminLoginRes = await request(app).put('/api/auth').send(admin);
 });
 
 test('login', async () => {
@@ -21,6 +27,40 @@ test('login', async () => {
   expect(loginRes.body.user).toMatchObject(expectedUser);
 });
 
+test('update user', async () => {
+  const updateUser = {
+    id: registerRes.body.user.id,
+    email: testUser.email,
+    password: 'new_password'
+  };
+
+  const updateUserRes = await request(app).put(`/api/auth/${registerRes.body.user.id}`).set('Authorization', `Bearer ${adminLoginRes.body.token}`).send(updateUser);
+
+  expect(updateUserRes.status).toBe(200);
+  expect(updateUserRes.body.email).toBe(updateUser.email);
+});
+
+test('logout', async () => {
+  await request(app).put('/api/auth').send(testUser);
+  const logoutRes = await request(app).delete('/api/auth').set('Authorization', `Bearer ${testUserAuthToken}`).send(testUser);
+  
+  expect(logoutRes.status).toBe(200);
+  expect(logoutRes.body.message).toBe('logout successful')
+})
+
 function expectValidJwt(potentialJwt) {
   expect(potentialJwt).toMatch(/^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/);
+}
+
+function randomName() {
+  return Math.random().toString(36).substring(2, 12);
+}
+
+async function createAdminUser() {
+  let user = { password: 'toomanysecrets', roles: [{ role: Role.Admin }] };
+  user.name = randomName();
+  user.email = user.name + '@admin.com';
+
+  user = await DB.addUser(user);
+  return { ...user, password: 'toomanysecrets' };
 }
